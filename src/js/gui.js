26b08996efc071c394038601c9af04e23f8e4931
Frederico D. S. Reckziegel <fdsreckziegel@inf.ufpel.edu.br>
2023-03-22 00:52:33 -0300

    Add files via upload

---
+++ src/js/gui.js
@@ -0,0 +1,10 @@
+var config = { 
+  rotate: degToRad(0),
+  quantity: 1
+};
+
+const loadGUI = () => {
+  const gui = new dat.GUI();
+  gui.add(config, "rotate", 0, 20, 0.5);
+  gui.add(config, "quantity", 1, 10, 1);
+};
