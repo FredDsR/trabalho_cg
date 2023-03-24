a0e94d2c591b329ff032d2f2d0a24ed0e386250a
Frederico Reckziegel <freddalsoglio@gmail.com>
2023-03-24 13:25:31 -0300

    adiciona logica para importar objeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,108 @@
-function main() {
-  const { gl, meshProgramInfo } = initializeWorld();
-  
-  var fieldOfViewRadians = degToRad(60);
+function parseOBJ(text) {
+  // because indices are base 1 let's just fill in the 0th data
+  const objPositions = [[0, 0, 0]];
+  const objTexcoords = [[0, 0]];
+  const objNormals = [[0, 0, 0]];
 
-  const cubeTranslation = [0, 0, 0];
+  // same order as `f` indices
+  const objVertexData = [
+    objPositions,
+    objTexcoords,
+    objNormals,
+  ];
 
-  const cubeBufferInfo = flattenedPrimitives.createCubeBufferInfo(gl, 20);
+  // same order as `f` indices
+  let webglVertexData = [
+    [],   // positions
+    [],   // texcoords
+    [],   // normals
+  ];
 
-  const cubeVAO = twgl.createVAOFromBufferInfo(
-    gl,
-    meshProgramInfo,
-    cubeBufferInfo,
-  );
+  function newGeometry() {
+    // If there is an existing geometry and it's
+    // not empty then start a new one.
+    if (geometry && geometry.data.position.length) {
+      geometry = undefined;
+    }
+    setGeometry();
+  }
 
-  const cubeUniforms = {
-    u_colorMult: [1, 0.5, 0.5, 1],
-    u_matrix: m4.identity(),
+  function addVertex(vert) {
+    const ptn = vert.split('/');
+    ptn.forEach((objIndexStr, i) => {
+      if (!objIndexStr) {
+        return;
+      }
+      const objIndex = parseInt(objIndexStr);
+      const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+      webglVertexData[i].push(...objVertexData[i][index]);
+    });
+  }
+
+  const keywords = {
+    v(parts) {
+      objPositions.push(parts.map(parseFloat));
+    },
+    vn(parts) {
+      objNormals.push(parts.map(parseFloat));
+    },
+    vt(parts) {
+      // should check for missing v and extra w?
+      objTexcoords.push(parts.map(parseFloat));
+    },
+    f(parts) {
+      const numTriangles = parts.length - 2;
+      for (let tri = 0; tri < numTriangles; ++tri) {
+        addVertex(parts[0]);
+        addVertex(parts[tri + 1]);
+        addVertex(parts[tri + 2]);
+      }
+    },
   };
-  
-  cubeDistance = 30;
-  cubeMaxQuantityPerRow = 5;
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword); 
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  return {
+    position: webglVertexData[0],
+    texcoord: webglVertexData[1],
+    normal: webglVertexData[2],
+  };
+}
+
+async function main() {
+  const { gl, meshProgramInfo } = initializeWorld();
+
+  console.log('chegou aqui primeiro')
+  const response = await fetch('http://localhost:8000/junk_food.obj');
+  const text = await response.text();
+  const data = parseOBJ(text);
+
+  console.log('chegou aqui')
+
+  const bufferInfo = twgl.createBufferInfoFromArray(gl, data);
+
+  const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+
+  var fieldOfViewRadians = degToRad(60);
 
   function computeMatrix(viewProjectionMatrix, translation, yRotation) {
     var matrix = m4.translate(
@@ -33,18 +116,7 @@ function main() {
 
   loadGUI();
   
-  let then = 0;
-  let deltatime;
-  let timeTracker = 0;
-
-  cameraSpeed = 10;
-  var cameraPosition = [0, 0, 100];
-
-  function render(time) {
-    time *= 0.001;
-    deltatime = time - then;
-    then = time;
-
+  function render() {
     twgl.resizeCanvasToDisplaySize(gl.canvas);
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -55,15 +127,7 @@ function main() {
     var projectionMatrix = m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
 
     // Compute the camera's matrix using look at.
-
-    if (cameraState.shouldAnimate) {
-      if (time - timeTracker > cameraState.cameraTransitionDuration) {
-        cameraSpeed *= -1;
-        timeTracker = time;
-      }
-      
-      cameraPosition[0] += cameraSpeed * deltatime; 
-    }
+    var cameraPosition = [0, 0, 100];
     var target = [0, 0, 0];
     var up = [0, 1, 0];
     var cameraMatrix = m4.lookAt(cameraPosition, target, up);
@@ -75,35 +139,21 @@ function main() {
 
     gl.useProgram(meshProgramInfo.program);
 
-    
     // ------ Draw the cube --------
-    gl.bindVertexArray(cubeVAO);
-    
-    let j = cubeDistance, k = 0;
-    for (let i = 0; i < config.quantity; i++) {
 
-      cubeTranslation[0] = (k * cubeDistance);
-      cubeTranslation[1] = j;
+    // Setup all the needed attributes.
+    gl.bindVertexArray(vao);
 
-      // Setup all the needed attributes.
-      cubeUniforms.u_matrix = computeMatrix(
-        viewProjectionMatrix,
-        cubeTranslation,
-        config.rotate,
-      );
-  
-      // Set the uniforms we just computed
-      twgl.setUniforms(meshProgramInfo, cubeUniforms);
-  
-      twgl.drawBufferInfo(gl, cubeBufferInfo);
-      
-      k++;
-      if (k == cubeMaxQuantityPerRow) {
-        k = 0;
-        j -= cubeDistance;
-      }
-    }
+    uniforms.u_matrix = computeMatrix(
+      viewProjectionMatrix,
+      [0, 0, 0],
+      config.rotate,
+    );
 
+    // Set the uniforms we just computed
+    twgl.setUniforms(meshProgramInfo, {"u_matrix": viewProjectionMatrix});
+
+    twgl.drawBufferInfo(gl, bufferInfo);
 	requestAnimationFrame(render);
   }
      
